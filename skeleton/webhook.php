25df6cbec8d0653bfa1ed749e8939012d7f8b951
Stoney Jackson <dr.stoney@gmail.com>
2015-04-24 21:05:18 -0400

    webhook: generate secret key and detect paths

--- skeleton/webhook.php
+++ skeleton/webhook.php
@@ -2,12 +2,22 @@
 require('.dip/lib/DipWebHook.php');
 $webhook = new DipWebHook(array(
 
-    // Some random secret key. Give it to GitHub when you create the webhook.
-    'secret' => '',
+    // Give this secret key to GitHub when you create the webhook.
+    // This value was generated when dip created this project.
+    // Feel free to change it.
+    'secret' => 'SECRET_KEY',
 
     // These will be prepended to $PATH before any shell commands.
     // Use it to ensure dip and git are in your path.
-    'paths' => array(),
+    'paths' => array(
+
+        // Uncomment to include the detected path to git.
+        #'PATH_TO_GIT',
+
+        // Uncomment to include the detected path to dip.
+        #'PATH_TO_DIP',
+
+    ),
 
 ));
 $webhook->ProcessRequest();
